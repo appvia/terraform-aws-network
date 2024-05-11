b796d9df25ae9b806d1e7d19e532fd1c87cb6026
Rohith Jayawardene <gambol99@gmail.com>
2024-05-11 15:18:55 +0100

    chore: updating the example to be more useful (#11)

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -8,13 +8,13 @@ variable "availability_zones" {
 variable "enable_ipam" {
   description = "Indicates the cidr block for the network should be assigned from IPAM"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "enable_transit_gateway" {
   description = "Indicates the network should provison nat gateways"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "enable_ssm" {
@@ -23,19 +23,22 @@ variable "enable_ssm" {
   default     = false
 }
 
+variable "ipam_pool_id" {
+  description = "The id of the IPAM pool to use for the network"
+  type        = string
+  default     = null
+}
+
 variable "name" {
   description = "Is the name of the network to provision"
   type        = string
+  default     = "test-vpc"
 }
 
 variable "private_subnet_netmask" {
   description = "The netmask for the private subnets"
   type        = number
-
-  validation {
-    condition     = var.private_subnet_netmask > 0 && var.private_subnet_netmask <= 28
-    error_message = "private_subnet_netmask must be between 1 and 28"
-  }
+  default     = 24
 }
 
 variable "public_subnet_netmask" {
@@ -47,16 +50,27 @@ variable "public_subnet_netmask" {
 variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
+  default = {
+    "Environment" = "test"
+    "GitRepo"     = "https://github.com/appvia/terraform-aws-network"
+    "Terraform"   = "true"
+  }
 }
 
 variable "transit_gateway_id" {
   description = "If enabled, and not lookup is disabled, the transit gateway id to connect to"
   type        = string
-  default     = ""
+  default     = "tgw-04ad8f026be8b7eb6"
 }
 
 variable "vpc_cidr" {
   description = "An optional cidr block to assign to the VPC (if not using IPAM)"
   type        = string
+  default     = "10.88.0.0/21"
+}
+
+variable "vpc_netmask" {
+  description = "The netmask for the VPC when using IPAM"
+  type        = number
   default     = null
 }
